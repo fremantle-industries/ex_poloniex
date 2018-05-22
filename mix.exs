b67dadda5ce814fb75e4a5845c51422f3163ca5a
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-05-21 21:32:57 -0700

    Release v0.0.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExPoloniex.Mixfile do
   def project do
     [
       app: :ex_poloniex,
-      version: "0.0.3",
+      version: "0.0.4",
       elixir: "~> 1.5",
       package: package(),
       start_permanent: Mix.env() == :prod,
